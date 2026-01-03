bd6dbc2eff1511f0c61a6b097fc8a0bb69b14684
EraKerceli <era.kerceli@student.uni-pr.edu>
2026-01-03 21:49:56 +0100

    Update create-event.js
    updated version plus lidhja me my event

--- create-event.js
+++ create-event.js
@@ -1,5 +1,31 @@
-document.getElementById("eventForm").addEventListener("submit", function (e) {
+$("#eventForm").on("submit", function (e) {
   e.preventDefault();
-  alert("Eventi u krijua me sukses!");
-  this.reset();
+
+  const event = {
+    title: $("#title").val(),
+    city: $("#city").val(),
+    contact: $("#contact").val(),
+    date: $("#date").val(),
+    maxPeople: $("#maxPeople").val()
+  };
+
+  
+  let events = JSON.parse(localStorage.getItem("myEvents")) || [];
+
+
+  events.push(event);
+
+  
+  localStorage.setItem("myEvents", JSON.stringify(events));
+
+  
+  alert("Eventi u krijua me sukses 🎉");
+
+  
+  window.location.href = "my-events.html";
+});
+
+
+$("#contact").on("input", function () {
+  this.value = this.value.replace(/[^0-9+]/g, "");
 });
